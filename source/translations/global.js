6771be91074fdbf713f320a636095d26f8aa49f0
obovoid <git.obovoid@gmail.com>
2023-12-13 20:46:53 +0100

    added first translations

---
+++ source/translations/global.js
@@ -0,0 +1,10 @@
+const language = {
+    "en-US": {
+        window_title: "OboStory",
+        project_startup_title: "Select an Action",
+        hint_recent_projects: "open your recent projects",
+        hint_create_project: "create a new project",
+        hint_goto_settings: "change the application settings",
+        hint_close_application: "close this application",
+    }
+}
