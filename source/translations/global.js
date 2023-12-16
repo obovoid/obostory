d78ff0df65b6bf98261578a1d4100b57d97c7b87
obovoid <git.obovoid@gmail.com>
2023-12-16 21:00:42 +0100

    translations

--- source/translations/global.js
+++ source/translations/global.js
@@ -1,10 +1,11 @@
 const language = {
     "en-US": {
         window_title: "OboStory",
-        project_startup_title: "Select an Action",
+        project_startup_title: "select an action",
         hint_recent_projects: "open your recent projects",
         hint_create_project: "create a new project",
         hint_goto_settings: "change the application settings",
         hint_close_application: "close this application",
+        settings_startup_title: "settings"
     }
 }
