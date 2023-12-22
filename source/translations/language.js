30808804519d157a856fdabc81b3045f1fb47267
obovoid <git.obovoid@gmail.com>
2023-12-22 23:57:19 +0100

    added new translations

---
+++ source/translations/language.js
@@ -0,0 +1,17 @@
+const language = {
+    "en-US": {
+        "window.title": "OboStory",
+        "renderer.modal.header": "select an action",
+        "hints.modal.recent.projects": "open your recent projects",
+        "hints.modal.create.project": "create a new project",
+        "hints.modal.settings": "change the application settings",
+        "hints.close.application": "close this application",
+        "settings.header": "settings",
+        "settings.categories.general": "general",
+        "settings.categories.timeline": "timeline",
+        "settings.categories.profiles": "profiles",
+        "settings.categories.fonts": "fonts",
+        "settings.categories.autocollapse.title": "Automatic Settings Collapsing",
+        "settings.categories.autocollapse.description": "Hide automatically listed sub-items in the categories instead of displaying them automatically",
+    }
+}
