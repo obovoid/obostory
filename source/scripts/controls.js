bd197a0f7ed52620d3919b10f4fef34f059024d1
obovoid <git.obovoid@gmail.com>
2023-12-29 19:19:49 +0100

    imports, copyright notice

--- source/scripts/controls.js
+++ source/scripts/controls.js
@@ -1,3 +1,12 @@
+/* 
+    Copyright (c) 2023, Obovoid
+    All rights reserved.
+
+    This source code is licensed under the GPL-3.0-style license found in the
+    LICENSE file in the root directory of this source tree.
+*/
+
+import { global } from "./global.js";
 onkeydown = (e) => {
     switch (e.key) {
         case 'Escape':
