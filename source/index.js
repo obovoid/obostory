59ae8b69db77c4426eea3bc181343fa0f6ac1cb9
obovoid <git.obovoid@gmail.com>
2023-12-29 19:15:05 +0100

    changed scripts to be module loaded

---
+++ source/index.js
@@ -0,0 +1,13 @@
+/* 
+    Copyright (c) 2023, Obovoid
+    All rights reserved.
+
+    This source code is licensed under the GPL-3.0-style license found in the
+    LICENSE file in the root directory of this source tree.
+*/
+
+import './renderer.js'
+import './scripts/global.js'
+import './translations/language.js'
+import './scripts/settings.js'
+import './scripts/controls.js'
