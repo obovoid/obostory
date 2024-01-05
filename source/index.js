749271cd115d83dd8698a8eecadd4b60ac444ba8
obovoid <git.obovoid@gmail.com>
2024-01-05 10:30:09 +0100

    client storage caching added

--- source/index.js
+++ source/index.js
@@ -1,10 +1,23 @@
 /* 
-    Copyright (c) 2023, Obovoid
+    Copyright (c) 2024, Obovoid
     All rights reserved.
 
     This source code is licensed under the GPL-3.0-style license found in the
     LICENSE file in the root directory of this source tree.
 */
+
+import { saveAppCache } from './components/cache/storage.js';
+import { app } from './renderer.js';
+
+async function init() {
+    const in_app = app(async () => {
+        const cache = await window.API.getStorageKey('app');
+
+        saveAppCache(cache);
+    })
+    if (!in_app) return console.warn('Cache initializing aborted. Not in app environment.')
+}
+await init();
 
 import './renderer.js'
 import './scripts/global.js'
