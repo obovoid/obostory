661cbfe31f74bd6533e3838ef6f345898ff6fc21
obovoid <git.obovoid@gmail.com>
2024-01-17 19:06:45 +0100

    branding logo, made path selection prompt, made project storing system with example, serverside translations

--- source/main.js
+++ source/main.js
@@ -7,10 +7,18 @@
 */
 'use strict';
 
-const {app, BrowserWindow, ipcMain, dialog, shell, globalShortcut} = require('electron');
+// eslint-disable-next-line no-unused-vars
+const {app, BrowserWindow, ipcMain, dialog, shell, globalShortcut, Menu} = require('electron');
 const path = require('node:path');
 const storage = require('electron-store');
-const Storage = new storage();
+const processStorage = new storage();
+const storagePath = getUserSelectedPath() || app.getPath('userData');
+console.log(`Starting App in Storage: ${storagePath}`);
+
+const Storage = new storage({cwd: storagePath});
+
+const fs = require('fs');
+const crypto = require('crypto');
 
 process.on('uncaughtException', error => {
 	console.error('Exception:', error);
@@ -29,13 +37,13 @@ function getWindowBounds() {
         return prefix_bounds;
     }
 
-    const bounds = Storage.get('windowBounds');
+    const bounds = processStorage.get('windowBounds');
 
     if (bounds) {
         return bounds;
     }
 
-    Storage.set('windowBounds', prefix_bounds);
+    processStorage.set('windowBounds', prefix_bounds);
     return prefix_bounds;
 }
 
@@ -48,7 +56,7 @@ function saveWindowBounds(size) {
     if (!Array.isArray(size) || size.length !== 2) {
         throw new Error(`Expected an array for the size argument, got ${typeof size} instead`);
     }
-    return Storage.set('windowBounds', size);
+    return processStorage.set('windowBounds', size);
 }
 
 /**
@@ -60,7 +68,7 @@ function saveWindowPosition(pos) {
     if (!Array.isArray(pos) || pos.length !== 2) {
         throw new Error(`Expected an array for the pos argument, got ${typeof pos} instead`);
     }
-    return Storage.set('windowPosition', pos);
+    return processStorage.set('windowPosition', pos);
 }
 
 /**
@@ -69,7 +77,7 @@ function saveWindowPosition(pos) {
  * @returns {number[]} The window position as an array of x and y in pixels.
  */
 function getWindowPosition() {
-    const position = Storage.get('windowPosition');
+    const position = processStorage.get('windowPosition');
 
     if (Storage.get('app.settings.storeWindowPosition') == false) {
         return null;
@@ -79,7 +87,8 @@ function getWindowPosition() {
     return null;
 }
 
-const WINDOW = () => {
+let global_window;
+const WINDOW = async () => {
     // window default settings. Overwritten by user, if changed any time.
 
     const [boundsWidth, boundsHeight] = getWindowBounds();
@@ -90,8 +99,23 @@ const WINDOW = () => {
             preload: path.join(__dirname, 'bridge.js'),
             nodeIntegration: false,
             contextIsolation: true
-        }
+        },
+        icon: path.join(__dirname, 'branding', 'icon2.png')
     });
+
+    // template for the menu
+    // const menu = Menu.buildFromTemplate([
+    //     {
+    //         label: app.name,
+    //         submenu: [
+    //             {
+    //                 click: () => win.webContents.send('receiver', 'quit'),
+    //                 label: 'Quit'
+    //             }
+    //         ]
+    //     }
+    // ]);
+    // Menu.setApplicationMenu(menu);
 
     const window_position = getWindowPosition()
     if (window_position) {
@@ -107,14 +131,50 @@ const WINDOW = () => {
     // load app
     win.loadFile(path.join(__dirname, 'app.html'));
 
+
+    const userSelectedPath = getUserSelectedPath();
+    if (!userSelectedPath) {
+        // Await to not register reload shortcut.
+        await showPathSelectionQuestionDialog();
+    }
+
+
     // register shortcuts;
 
     globalShortcut.register('f5', function() {
         console.log('reloading environment...');
         win.reload();
     })
+    global_window = win;
 }
 
+function getUserSelectedPath() {
+    return processStorage.get("storagePath");
+}
+
+async function showPathSelectionQuestionDialog() { 
+    const messageBoxOptions = {
+        type: "question",
+        title: "Storage Path Selection",
+        message: `You have not selected a storage path yet. Do you want to select one, or do you want to use the default storage path at\n${storagePath}?`,
+        buttons: ['change storage path', 'use default storage path']
+    }
+
+    const { response } = await dialog.showMessageBox(messageBoxOptions);
+    if (response == 1) return;
+
+    const userSelectedPath = await dialog.showOpenDialog({
+        properties: ['openDirectory'],
+        defaultPath: storagePath
+    });
+    if (!userSelectedPath.canceled) {
+        processStorage.set("storagePath", userSelectedPath.filePaths[0]);
+        return true
+    } else {
+        processStorage.set("storagePath", storagePath);
+        return false
+    }
+}
 
 // Create Window on app startup
 app.whenReady().then(() => {
@@ -193,6 +253,93 @@ ipcMain.on('requestOpenUrl', async (_event, url) => {
     return openURL(url);
 });
 
+String.prototype.format = function() {
+    let args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) {
+        return typeof args[number] != 'undefined' ? args[number] : match;
+    });
+}
+
+ipcMain.on('requestRestart', async (_event, options = {}) => {
+    const opts = options;
+    // Wait to finish all tasks before restarting the app.
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();    
+        }, 100)
+    });
+
+    const title = await contextIdToMessage("ipc.requestRestart.title") || "invalid";
+    const message = await contextIdToMessage("ipc.requestRestart.message") || "invalid";
+    const only_once = await contextIdToMessage("ipc.requestRestart.onlyOnce") || "invalid";
+    const agree = await contextIdToMessage("ipc.requestRestart.agree") || "invalid";
+    const disagree = await contextIdToMessage("ipc.requestRestart.disagree") || "invalid";
+    const messageBoxOptions = {
+        type: "question",
+        title: title,
+        message: String(message).format(opts.once ? only_once : ""),
+        buttons: [agree, disagree]
+    }
+
+    const { response } = await dialog.showMessageBox(messageBoxOptions);
+    if (response == 1) return;
+
+    return app.relaunch(), app.exit(1);
+});
+
+ipcMain.on('requestSaveProject', async (_event, project, content) => {
+    writeProjectToDisk(project, content);
+    return true;
+});
+
+ipcMain.handle('requestLoadProject', async (_event, project = null) => {
+    if (project) {
+        const project_data = readProjectFromDisk(project);
+        if (!project_data) throw new Error(`Project "${project}" does not exist`);
+        return project_data;
+    }
+
+    const userSelectedFile = await dialog.showOpenDialog({
+        properties: ['openFile'],
+        defaultPath: path.join(storagePath, 'projects'),
+        filters: [
+            { name: 'OSP', extensions: ['osp'] }
+        ]
+    });
+
+    if (userSelectedFile.canceled) return null;
+    console.log(`Loading project from "${userSelectedFile.filePaths[0]}"`);
+});
+
+function writeProjectToDisk(project, content) {
+    const dir_exists = fs.existsSync(path.join(storagePath, 'projects'));
+    if (!dir_exists) {
+        fs.mkdirSync(path.join(storagePath, 'projects'));
+    }
+
+    const buffer = stringToBuffer(content);
+    fs.writeFileSync(path.join(storagePath, 'projects', `${project}.osp`), buffer, (err) => {
+        if (err) throw err;
+    });
+}
+
+function readProjectFromDisk(project) {
+    let buffer = fs.readFileSync(path.join(storagePath, 'projects', `${project}.osp`), { encoding: 'utf8' });
+    return bufferToString(buffer);
+}
+
+// writeProjectToDisk('test', 'project.set.title@sometitle project.create.profile@profile_name profile_name.set.birthdate@1990-01-01');
+// console.log(readProjectFromDisk('test'));
+
+function stringToBuffer(string) {
+    return new Buffer.from(string).toString('base64');
+}
+
+function bufferToString(buffer) {
+    const decodedBuffer = new Buffer.from(buffer, 'base64');
+    return decodedBuffer.toString();
+}
+
 ipcMain.on('showAppInfo', () => {
     const messageBoxOptions = {
         type: "info",
@@ -201,4 +348,41 @@ ipcMain.on('showAppInfo', () => {
     }
     dialog.showMessageBoxSync(messageBoxOptions);
     return true;
-})
+});
+
+// generate a unique id with the length of 16 characters including all utf-8 characters.
+function generateUniqueId() {
+    return crypto.randomBytes(16).toString('hex');
+}
+
+/**
+ * Sends a message to the Bridge process and returns a response.
+ * @param {string} actionMessage - The message to send to the Bridge process.
+ * @param {any} [customParameter] - Optional parameter to send to the Bridge process.
+ * @returns {Promise<any>} A promise that resolves with the response from the Bridge process.
+ */
+async function sendMessageToBridge(actionMessage, customParameter = null) {
+  return new Promise((resolve, reject) => {
+    const id = generateUniqueId();
+    let resolved = false;
+
+    ipcMain.once(id, (_event, data) => {
+      resolve(data);
+      resolved = true;
+    });
+
+    global_window.webContents.send('receiver', actionMessage, customParameter, id);
+
+    setTimeout(() => {
+      if (!resolved) {
+        ipcMain.removeHandler(id);
+        reject(new Error('Timeout'));
+      }
+    }, 8000);
+  });
+}
+
+async function contextIdToMessage(contextId) {
+    const string_result = await sendMessageToBridge('translateContextId', contextId);
+    return string_result;
+}
