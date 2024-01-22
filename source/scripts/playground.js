1f9861141eb16f404b7a9db7da4154de8d28e3fa
obovoid <git.obovoid@gmail.com>
2024-01-22 18:46:53 +0100

    copyright notice

--- source/scripts/playground.js
+++ source/scripts/playground.js
@@ -1,3 +1,10 @@
+/* 
+    Copyright (c) 2024, Obovoid
+    All rights reserved.
+
+    This source code is licensed under the GPL-3.0-style license found in the
+    LICENSE file in the root directory of this source tree.
+*/
 
 import { onAction, sendAction, getActivePage, loadNewPage } from "../renderer.js";
 import { global } from "../scripts/global.js"
